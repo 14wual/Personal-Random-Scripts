8644ff76c6e7924a2605b1d9e12d336c926e963d
Wual1014 <105047274+14wual@users.noreply.github.com>
2023-01-08 20:54:25 +0100

    Update __init__.py

--- Scripts/Task/__init__.py
+++ Scripts/Task/__init__.py
@@ -1,8 +1,22 @@
+# ██╗    ██╗██╗   ██╗ █████╗ ██╗     
+# ██║    ██║██║   ██║██╔══██╗██║     
+# ██║ █╗ ██║██║   ██║███████║██║     (code by wual)
+# ██║███╗██║██║   ██║██╔══██║██║     
+# ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
+#  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
+
+# Tasks
+# See proyect >> https://github.com/14wual/Personal-Random-Scripts/tree/main/Scripts/Task
+# Follow me >> https://twitter.com/codewual
+
+# --------------------Extern Imports--------------------
 import customtkinter
 
+# ----------Intern Imports--------------------
+from pages import today
+from pages import add
 
-from pages import main
-
+# --------------------APP--------------------
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -10,7 +24,11 @@ class App(customtkinter.CTk):
         self.title("CTk example")
         customtkinter.set_appearance_mode("light")
 
-        main.main(self)
+        self.main_frame = customtkinter.CTkFrame(self)
+        self.main_frame.pack(padx=5, pady=5)
+        
+        today.tasks(self)
+        add.add_task(self)
 
 if __name__ == '__main__':
     app = App()
